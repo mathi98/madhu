2bb9ad931d0d41402782f8c5940bfe14f57db96d
mathi98 <50616569+mathi98@users.noreply.github.com>
2019-05-14 15:42:08 +0530

    Add files via upload

---
+++ operator.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+int main() 
+{
+int num;
+
+printf("Enter the num");
+
+scanf("%d",&num);
+if(num>=0)
+	
+printf("%d is a positive num\n",num);
+
+else if(num==0)
+
+ printf("%d is a zero num\n",num);
+
+ else
+ 
+printf("%d is a negative num\n",num);
+
+	
+return 0;
+}
