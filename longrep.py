4ab207f0a1bbd4021ea8b00b0997ee7f7853960e
mathi98 <50616569+mathi98@users.noreply.github.com>
2019-06-04 18:52:56 +0530

    Create longrep.py

---
+++ longrep.py
@@ -0,0 +1,14 @@
+k=int(input())
+l=list(map(int,input().split()))
+c=1
+m=c
+f=1
+for i in range(k-1):
+    if l[i]==l[i+1]:
+        c+=1
+        f=c
+    elif c>m:
+        m=c
+        f=c
+        c=1
+print(f)
